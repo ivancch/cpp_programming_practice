a17fd3eef355180a6f2b644dfef413dd034e880c
Ivan- <76176527+ivancch@users.noreply.github.com>
2023-07-27 03:18:46 +0300

    creating a project template

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : c++_programming_practice.cpp
+// Author      : ivan-ch
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include <iostream>
+#include <conio.h>
+
+int main() {
+	std::cout << "Start of programs\n\n";
+
+	std::cout << "\n\nEnd of program" << std::endl;
+	_getch();
+	return 0;
+}
---
+++ src/myincludes1.h
@@ -0,0 +1,14 @@
+/**
+ * myincludes1.h
+ *
+ *
+ */
+
+#ifndef MYINCLUDES1_H_
+#define MYINCLUDES1_H_
+
+
+
+
+
+#endif /* MYINCLUDES1_H_ */
